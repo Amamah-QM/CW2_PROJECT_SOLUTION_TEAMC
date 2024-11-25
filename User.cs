22661badf3638d38c8b2fb2aa140ac26cdb6c414
haehhb <Abdelrahman-H@Ulster.ac.uk>
2024-11-25 15:14:44 +0300

    Update User.cs
    Add User class with basic properties

--- User.cs
+++ User.cs
@@ -1,1 +1,9 @@
+using System;
 
+public class User
+{
+    // Basic user information for account management and authentication
+    public int UserId { get; set; }
+    public string UserName { get; set; }
+    public string Password { get; set; }
+    public string UserEmail { get; set; }
