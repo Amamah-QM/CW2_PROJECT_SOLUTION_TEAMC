3e34eef1ddc514efacd7ef6a6d7dc5cae761dd2c
haehhb <Abdelrahman-H@Ulster.ac.uk>
2024-11-25 15:18:41 +0300

    Add contact and address fields to User class

--- User.cs
+++ User.cs
@@ -7,3 +7,9 @@ public class User
     public string UserName { get; set; }
     public string Password { get; set; }
     public string UserEmail { get; set; }
+    
+   // Contact and shipping address information
+    public string PhoneNumber { get; set; }
+    public string Street { get; set; }
+    public string City { get; set; }
+    public string Postcode { get; set; }
