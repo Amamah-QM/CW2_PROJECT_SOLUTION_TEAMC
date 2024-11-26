023106daea7305d08c37c96d795afb5133f14dc6
haehhb <Abdelrahman-H@Ulster.ac.uk>
2024-11-26 22:33:33 +0300

    Create Category class
    Created the Category class to represent product categories in the system. This class includes properties such as CategoryId, CategoryName, and CategoryDescription, laying the foundation for organizing products into distinct categories.

---
+++ Category.cs
@@ -0,0 +1,7 @@
+public class Category
+{
+    // Basic category information for product organization
+    public int CategoryId { get; set; }
+    public string CategoryName { get; set; }
+    public string CategoryDescription { get; set; }
+   
