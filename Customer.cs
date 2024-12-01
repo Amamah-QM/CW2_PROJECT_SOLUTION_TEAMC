6692d774e93811be00783426233862f738eebc3c
Khushi-Sahh <Sah-k@ulster.ac.uk>
2024-12-01 14:18:44 +0300

    Create Customer.cs
    Created the customer class

---
+++ Customer.cs
@@ -0,0 +1,8 @@
+public class Customer
+{
+    public string Role { get; set; }
+    public string Status { get; set; }
+
+    public void CreateSampleCustomers() { }
+    public void DisplayCustomerMenu() { }
+}
