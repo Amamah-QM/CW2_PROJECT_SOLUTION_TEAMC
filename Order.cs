d0bcbbd33c77750956eb0ad53cebf3c63d3b1cc4
IMN17 <150557830+IMN17@users.noreply.github.com>
2024-12-03 23:35:46 +0300

    Update Order.cs
    Included the System namespace and System.Collections.Generic namespace

--- Order.cs
+++ Order.cs
@@ -1,5 +1,8 @@
- public class Order
-    {
+using System;
+using System.Collections.Generic;
+
+public class Order
+{
         public int OrderId { get; set; }
         public DateTime DateOrdered { get; set; }
         public string OrderStatus { get; set; }
@@ -10,4 +13,4 @@
             DateOrdered = dateOrdered;
             OrderStatus = orderStatus;
         }
-    }
+}
