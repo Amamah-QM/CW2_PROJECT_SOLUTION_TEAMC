1c0ad930eefdaee1bf0cabd00067132620776653
IMN17 <150557830+IMN17@users.noreply.github.com>
2024-12-03 23:33:07 +0300

    Update Category.cs
    Changed access modifiers to private

--- Category.cs
+++ Category.cs
@@ -4,9 +4,9 @@ using System.Collections.Generic;
 public class Category
 {
     // Basic category information for product organization
-    public int CategoryId { get; set; }
-    public string CategoryName { get; set; }
-    public string CategoryDescription { get; set; }
+    private int CategoryId { get; set; }
+    private string CategoryName { get; set; }
+    private string CategoryDescription { get; set; }
    
     // Creates a new category with required information
     public Category(int categoryId, string categoryName, string categoryDescription)
